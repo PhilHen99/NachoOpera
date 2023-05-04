c0ab0c23611c8588145a7b07f1dd27f818ac197f
PhilHen99 <juan.henao358@pascualbravo.edu.co>
2023-05-03 21:08:17 -0500

    Initial commit

---
+++ Integrado/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Integrado
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            long resultM, resultD, resultS, resultR;
+            resultS = NachoOperaSyR.Class1.Suma(53,32);
+            resultR = NachoOperaSyR.Class1.Resta(12, 4);
+            resultM = NachoOperaMyD.Class1.Multiplicacion(3,4);
+            resultD = NachoOperaMyD.Class1.Division(10, 2);
+            Console.WriteLine(resultS.ToString());
+            Console.WriteLine(resultR.ToString());
+            Console.WriteLine(resultM.ToString());
+            Console.WriteLine(resultD.ToString());
+            Console.ReadKey();
+        }
+    }
+}
